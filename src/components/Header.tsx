b8d75b253ce15b9e1825c1da6706b9ca142b7a94
Rafael Teles Vital <rafael@Rafaels-MacBook-Pro.local>
2024-04-18 10:32:32 -0300

    conection with API

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,19 @@
+import { useAppSelector } from "../store"
+import { useCurrentLesson } from "../store/slices/player"
+
 export function Header() {
+  const { currentLesson, currentModule } = useCurrentLesson()
+
+  const isCourseLoading = useAppSelector(state => state.player.isLoading)
+
+  if (isCourseLoading) {
+    return <h1 className="text-2xl font-bold">Carregando...</h1>
+  }
+
   return (
     <div className="flex flex-col gap-1">
-      <h1 className="text-2xl font-bold">Fundamentos do Redux</h1>
-      <span className="text-sm text-zinc-400">Módulo "Desendando o redux"</span>
+      <h1 className="text-2xl font-bold">{currentLesson?.title}</h1>
+      <span className="text-sm text-zinc-400">{currentModule?.title}</span>
     </div>
   )
 }
--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -1,5 +1,8 @@
+import * as Collapsible from '@radix-ui/react-collapsible'
 import { ChevronDown } from "lucide-react";
 import { Lesson } from "./Lesson";
+import { useAppDispatch, useAppSelector } from '../store';
+import { play } from '../store/slices/player';
 
 interface ModuleProps {
   moduleIndex: number
@@ -8,9 +11,21 @@ interface ModuleProps {
 }
 
 export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
+  const dispatch = useAppDispatch()
+
+  const { currentModuleIndex, currentLessonIndex } = useAppSelector(state => {
+    const { currentModuleIndex, currentLessonIndex } = state.player
+
+    return { currentModuleIndex, currentLessonIndex }
+  })
+
+  const lessons = useAppSelector(state => {
+    return state.player.course?.modules[moduleIndex].lessons
+  })
+
   return (
-    <div>
-      <button className="flex w-full items-center gap-3 bg-zinc-800 p-4">
+    <Collapsible.Root className='group' defaultOpen={moduleIndex === 0}>
+      <Collapsible.Trigger className="flex w-full items-center gap-3 bg-zinc-800 p-4">
         <div className="flex h-10 w-10 rounded-full items-center justify-center bg-zinc-950 text-xs">
           {moduleIndex + 1}
         </div>
@@ -20,14 +35,28 @@ export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
           <span className="text-xs text-zinc-400">{amountOfLessons} aulas</span>
         </div>
 
-        <ChevronDown className="w-4 h-4 ml-auto text-zinc-400" />
-      </button>
+        <ChevronDown className="w-4 h-4 ml-auto text-zinc-400 group-data-[state=open]:rotate-180 transition-transform" />
+      </Collapsible.Trigger>
 
-      <nav className="relative flex flex-col gap-4 p-6">
-        <Lesson title="Funcdamentos do Redux" duration="09:17"/>        
-        <Lesson title="Funcdamentos do Redux" duration="09:17"/>        
-        <Lesson title="Funcdamentos do Redux" duration="09:17"/>                
-      </nav>
-    </div>
+      <Collapsible.Content>
+        <nav className="relative flex flex-col gap-4 p-6">
+          {lessons && lessons.map((lesson, lessonIndex) => {
+            
+            const isCurrent = currentModuleIndex === moduleIndex &&
+            currentLessonIndex === lessonIndex
+
+            return (
+              <Lesson
+                key={lesson.id}
+                title={lesson.title}
+                duration={lesson.duration}  
+                onPlay={() => dispatch(play([moduleIndex, lessonIndex]))}    
+                isCurrent={isCurrent}        
+              />
+            )
+          })}
+        </nav>
+      </Collapsible.Content>
+    </Collapsible.Root>
   )
 }
