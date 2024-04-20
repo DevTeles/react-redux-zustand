f25220177eb26a7ed24c913348186f00e8492cda
Rafael Teles Vital <rafael@Rafaels-MacBook-Pro.local>
2024-04-20 19:40:35 -0300

    fixed in the player json-server

--- src/store/slices/player.ts
+++ src/store/slices/player.ts
@@ -32,9 +32,8 @@ const initialState: PlayerState = {
 export const loadCourse = createAsyncThunk(
   'player/load',
   async () => {
-    const response = await api.get('/courses/1')
-    
-    return response.data
+    const response = await api.get('/server.json?key=486757c0')  
+    return response.data.courses[0]
   }
 )
 
